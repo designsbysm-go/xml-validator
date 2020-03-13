96c98ec955f43e3c32a0945acc506ab9844f50d2
Scott A. Matthews <smatthews@starwesttech.com>
2020-03-13 11:19:40 -0700

    remove unused code

--- main.go
+++ main.go
@@ -10,21 +10,6 @@ import (
 
 // TODO: add unit tests
 
-/* func readFileToBuffer(path string) ([]byte, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	buffer, err := ioutil.ReadAll(file)
-	if err != nil {
-		return nil, err
-	}
-
-	return buffer, nil
-} */
-
 func main() {
 	c := Config{}
 	c.Setup()
